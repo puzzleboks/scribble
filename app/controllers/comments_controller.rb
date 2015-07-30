bfb71cec77d3f8efdcb739d32bfd9bb80ad8b8ee
lon phelps <puzzleboks@gmail.com>
2015-07-29 21:41:57 -0400

    created comments controller with nested routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,57 @@
 class CommentsController < ApplicationController
   # index
   def index
-    @comments = Comment.all
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.all
+  end
+
+  # new
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  # create
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    redirect_to post_comments_path(@post, @comment)
+  end
+
+  #show
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  # edit
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  # update
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+
+    redirect_to post_comments_path(@post, @comment)
+  end
+
+  # destroy
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_comments_path( @post )
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author, :text, :created_at, :updated_at, :post_id)
   end
 
 end
